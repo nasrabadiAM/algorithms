837854ad737200e05ec2660ace332a20e9b97e6b
Ali Nasrabadi <nasrabadiam@gmail.com>
2024-07-13 07:53:18 +0330

    Implement my first solution for Valid anagram
    I used a mutable list and a for loop over s, so its O(n) and its memory usage
    I implement this question also with stringBuilder and hashmap

--- src/main/kotlin/easy/ValidAnagram.kt
+++ src/main/kotlin/easy/ValidAnagram.kt
@@ -5,8 +5,84 @@ package org.example.easy
  * @see easy.ValidAnagramTest.kt
  * */
 
-class ValidAnagram {
-    fun isAnagram(s: String, t: String): Boolean {
+interface ValidAnagram {
+    fun isAnagram(s: String, t: String): Boolean
+}
+
+/**
+ * duration: 575
+ * memory: 37mb
+ */
+class ValidAnagramArrayImpl : ValidAnagram {
+    override fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        if (s.isEmpty() && t.isEmpty()) return true
+        var target = t.toMutableList()
+        for (char in s) {
+            val index = target.indexOf(char)
+            if (index == -1) return false
+            target.removeAt(index)
+        }
+        if (target.isEmpty()) return true
         return false
     }
 }
+
+/**
+ * duration: 456
+ * memory: 38.5mb
+ */
+class ValidAnagramStringBuilderImpl : ValidAnagram {
+    override fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        if (s.isEmpty() && t.isEmpty()) return true
+        val stringBuilder = StringBuilder(t)
+        for (char in s) {
+            val index = stringBuilder.indexOf(char)
+            if (index == -1) return false
+            stringBuilder.deleteAt(index)
+        }
+        if (stringBuilder.isEmpty()) return true
+        return false
+    }
+}
+
+/**
+ * duration: 210
+ * memory: 37.5mb
+ */
+class ValidAnagramHashMapImpl : ValidAnagram {
+    override fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        if (s.isEmpty() && t.isEmpty()) return true
+        val hashMap = HashMap<Char, Int>()
+        for (char in s) {
+            val currentValue = hashMap[char]
+            if (currentValue != null) {
+                hashMap[char] = currentValue + 1
+            } else {
+                hashMap[char] = 1
+            }
+        }
+        for (char in t) {
+            val currentValue = hashMap[char]
+            if (currentValue != null) {
+                if (currentValue == 1) {
+                    hashMap.remove(char)
+                } else {
+                    hashMap[char] = currentValue - 1
+                }
+            } else {
+                return false
+            }
+        }
+        if (hashMap.isEmpty()) return true
+        return false
+    }
+}
+
+/**
+ * best solution time: Using IntArray + unicode calculations
+ *
+ * best solution memory: Using IntArray + unicode calculations
+ */
--- src/test/kotlin/easy/ValidAnagramTest.kt
+++ src/test/kotlin/easy/ValidAnagramTest.kt
@@ -1,26 +1,60 @@
 package easy
 
-import org.example.easy.ValidAnagram
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import org.example.easy.*
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class ValidAnagramTest {
+    private val validAnagram: ValidAnagram = ValidAnagramHashMapImpl()
+    // private val validAnagram: ValidAnagram = ValidAnagramStringBuilderImpl()
+    // private val validAnagram: ValidAnagram = ValidAnagramArrayImpl()
 
-    @BeforeEach
-    fun setUp() {
-    }
-
-    @AfterEach
-    fun tearDown() {
+    @Test
+    fun whenTwoDifferentLengthShouldReturnFalse() {
+        val result = validAnagram.isAnagram("sdf", "d")
+        assertFalse(result)
     }
 
     @Test
-    fun isAnagram() {
-        val validAnagram = ValidAnagram()
-        val expected = validAnagram.isAnagram("", "")
-        assertEquals(expected, true)
+    fun whenTwoEmptyWordShouldReturnTrue() {
+        val result = validAnagram.isAnagram("", "")
+        assertTrue(result)
+    }
+
+    @Test
+    fun whenTwoLetterWordIsAnagramShouldReturnTrue() {
+        val result = validAnagram.isAnagram("hi", "ih")
+        assertTrue(result)
+    }
+
+    @Test
+    fun whenTwoLetterWordIsNotAnagramShouldReturnTrue() {
+        val result = validAnagram.isAnagram("hi", "if")
+        assertFalse(result)
+    }
+
+    @Test
+    fun whenThreeLetterWordIsNotAnagramShouldReturnTrue() {
+        val result = validAnagram.isAnagram("min", "man")
+        assertFalse(result)
+    }
+
+    @Test
+    fun whenThreeLetterWordIsAnagramShouldReturnTrue() {
+        val result = validAnagram.isAnagram("min", "nim")
+        assertTrue(result)
+    }
+
+    @Test
+    fun whenSevenLetterWordIsAnagramShouldReturnTrue() {
+        val result = validAnagram.isAnagram("mindset", "setmind")
+        assertTrue(result)
+    }
+
+    @Test
+    fun whenSevenLetterWordIsNotAnagramShouldReturnFalse() {
+        val result = validAnagram.isAnagram("mindset", "setminm")
+        assertFalse(result)
     }
 }
