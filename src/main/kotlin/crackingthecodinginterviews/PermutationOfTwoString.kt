2288eb73956aecf22d36432fd9e6fbdcb24b985e
Ali Nasrabadi <nasrabadiam@gmail.com>
2024-11-06 10:01:37 +0330

    Refactor tests and implement array for ascii code

--- src/main/kotlin/crackingthecodinginterviews/PermutationOfTwoString.kt
+++ src/main/kotlin/crackingthecodinginterviews/PermutationOfTwoString.kt
@@ -38,3 +38,26 @@ class HashMapSolution : PermutationOfTwoString {
         return map.isEmpty()
     }
 }
+
+/**
+ * If the char set is ASCII it has 256 chars in its set, so we can have
+ * an array of size 256 and put counts of each char in this array.
+ *
+ * time complexity: O(n)
+ * implementation time: 5 min
+ */
+class CharArraySolution : PermutationOfTwoString {
+
+    override fun isPermutation(first: String, second: String): Boolean {
+        val charArray = IntArray(256)
+        if (first.length != second.length) return false
+        for (char in first) {
+            charArray[char.code] = charArray[char.code] + 1
+        }
+        for (char in second) {
+            charArray[char.code] = charArray[char.code] - 1
+            if (charArray[char.code] < 0) return false
+        }
+        return charArray.all { it == 0 }
+    }
+}
--- src/test/kotlin/crackingthecodinginterviews/PermutationOfTwoString.kt
+++ src/test/kotlin/crackingthecodinginterviews/PermutationOfTwoString.kt
@@ -1,5 +1,6 @@
 package crackingthecodinginterviews
 
+import org.example.crackingthecodinginterviews.CharArraySolution
 import org.example.crackingthecodinginterviews.HashMapSolution
 import org.example.crackingthecodinginterviews.PermutationOfTwoString
 import org.junit.jupiter.api.Test
@@ -9,76 +10,96 @@ import kotlin.test.assertTrue
 class PermutationOfTwoStringTest {
 
     private val solution: PermutationOfTwoString = HashMapSolution()
+    private val solutionArray: PermutationOfTwoString = CharArraySolution()
 
     @Test
     fun shouldReturnFalseIfTwoStringAreNotSameSize() {
         val first = "abc"
         val second = "abcd"
-        val result = solution.isPermutation(first, second)
-        assertFalse(result, "<$first> is not permutation of <$second>")
+        solution.assertFalse(first, second)
+        solutionArray.assertFalse(first, second)
     }
 
     @Test
     fun shouldReturnTrueIfTwoStringAreEmpty() {
         val first = ""
         val second = ""
-        val result = solution.isPermutation(first, second)
-        assertTrue(result, "two empty strings are permute.")
+        solution.assertTrue(first, second)
+        solutionArray.assertTrue(first, second)
     }
 
     @Test
     fun shouldReturnTrueIfTwoStringArePermute() {
         val first = "abc"
         val second = "cba"
-        val result = solution.isPermutation(first, second)
-        assertTrue(result, "$first is not permutation of $second")
+        solution.assertTrue(first, second)
+        solutionArray.assertTrue(first, second)
     }
 
     @Test
     fun shouldReturnTrueIfTwoStringArePermuteAndHaveRepetitiveChars() {
         val first = "abccc"
         val second = "cbcca"
-        val result = solution.isPermutation(first, second)
-        assertTrue(result, "$first is not permutation of $second")
+        solution.assertTrue(first, second)
+        solutionArray.assertTrue(first, second)
     }
 
     @Test
     fun shouldReturnTrueIfOneRepetitiveCharIsMissing() {
         val first = "abcc"
         val second = "bcca"
-        val result = solution.isPermutation(first, second)
-        assertTrue(result, "$first is not permutation of $second")
+        solution.assertTrue(first, second)
+        solutionArray.assertTrue(first, second)
     }
 
     @Test
     fun shouldReturnFalseIfOneCharIsMissing() {
         val first = "abccd"
         val second = "bccax"
-        val result = solution.isPermutation(first, second)
-        assertFalse(result, "$first is permutation of $second")
+        solution.assertFalse(first, second)
+        solutionArray.assertFalse(first, second)
     }
 
     @Test
     fun shouldReturnFalseIfOneCharIsNotSame() {
         val first = "a"
         val second = "b"
-        val result = solution.isPermutation(first, second)
-        assertFalse(result, "$first is permutation of $second")
+        solution.assertFalse(first, second)
+        solutionArray.assertFalse(first, second)
     }
 
     @Test
     fun shouldReturnTrueIfTwoSameString() {
         val first = "Hello"
         val second = "Hello"
-        val result = solution.isPermutation(first, second)
-        assertTrue(result, "$first is not permutation of $second")
+        solution.assertTrue(first, second)
+        solutionArray.assertTrue(first, second)
     }
 
     @Test
     fun shouldReturnFalseIfTwoSameStringAreSameButCaseSensitive() {
         val first = "hello"
         val second = "Hello"
-        val result = solution.isPermutation(first, second)
-        assertFalse(result, "$first is permutation of $second")
+        solution.assertFalse(first, second)
+        solutionArray.assertFalse(first, second)
+    }
+
+    @Test
+    fun shouldReturnFalseIfTwoSameStringWithDifferentWhiteSpaces() {
+        val first = "hello      "
+        val second = "Hello"
+        solution.assertFalse(first, second)
+        solutionArray.assertFalse(first, second)
+    }
+
+
+    private fun PermutationOfTwoString.assertFalse(first: String, second: String) {
+        val result = this.isPermutation(first, second)
+        assertFalse(result, "<$first> is not permutation of <$second>, but you expect it is.")
+    }
+
+    private fun PermutationOfTwoString.assertTrue(first: String, second: String) {
+        val result = this.isPermutation(first, second)
+        assertTrue(result, "<$first> is permutation of <$second>, but you expect not.")
     }
 }
